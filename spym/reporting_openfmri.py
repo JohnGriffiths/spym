3211d9f7879c08670de18ba2bdf90f5ac19cbb09
Yannick Schwartz <ys218403@is220245.(none)>
2013-09-24 11:43:08 -0500

    added auto threshold with score at percentile and store nifti images as well

--- spym/reporting_openfmri.py
+++ spym/reporting_openfmri.py
@@ -8,6 +8,7 @@ import nibabel as nb
 import numpy as np
 import pylab as pl
 
+from scipy import stats
 from nipy.labs.viz import plot_map
 from nipy.labs.viz_tools import cm
 from joblib import Parallel, delayed
@@ -80,11 +81,15 @@ def _plot_group_map(label, individual_maps, out_dir):
     mean_map = np.zeros(mask.shape)
     mean_map[mask] = np.mean(individual_maps, axis=0)
     vmax = np.abs(mean_map).max()
+    threshold = stats.scoreatpercentile(mean_map, 80)
     plot_map(mean_map, affine, slicer='z',
              cut_coords=[-40, -20, -5, 0, 10, 30, 60],
-             vmin=-vmax, vmax=vmax, threshold=1.,
-             cmap=cm.cold_hot, title=label)
+             vmin=-vmax, vmax=vmax, threshold=threshold,
+             cmap=cm.cold_hot, title='%s scoreatper=%.2f' % (
+                 label, threshold))
     pl.savefig(os.path.join(out_dir, 'group_%s.png' % label), dpi=500)
+    img = nb.Nifti1Image(img, affine=affine)
+    nb.save(img, os.path.join(out_dir, 'group_%s.nii.gz' % label)
 
 
 if __name__ == '__main__':
